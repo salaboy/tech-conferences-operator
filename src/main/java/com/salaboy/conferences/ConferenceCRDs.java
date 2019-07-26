79836394cc77c69c16b12c4b9a8afa4a34991569
salaboy <salaboy@gmail.com>
2019-07-26 15:21:40 +0100

    adding crd

--- src/main/java/com/salaboy/conferences/ConferenceCRDs.java
+++ src/main/java/com/salaboy/conferences/ConferenceCRDs.java
@@ -1,7 +1,7 @@
 package com.salaboy.conferences;
 
 public class ConferenceCRDs {
-    public static String CONF_CRD_GROUP = "com.salaboy.conferences";
+    public static String CONF_CRD_GROUP = "alpha.k8s.salaboy.com";
     public static String CONF_CRD_NAME = "conferences." + CONF_CRD_GROUP;
 //    public static String MICROSERVICE_CRD_NAME = "microservices." + CONF_CRD_GROUP;
 //    public static String GATEWAY_CRD_NAME = "gateways." + CONF_CRD_GROUP;
